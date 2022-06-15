89ade68d0389ea74e655bf1a3c7d243949285ee7
Ethancao123 <ethan.cao123456gmail.comgit config --global user.email ethan.cao123456gmail.com>
2022-06-15 02:05:09 +0100

    detection works W

---
+++ main.py
@@ -0,0 +1,56 @@
+# Standard imports
+import cv2
+import numpy as np;
+
+# Read image
+cam = cv2.VideoCapture(0)
+# finds the vector the crosshair needs to move to reach the closest target
+def findMove(keypoints):
+    centerX = 315
+    centerY = 177
+    i = 0
+    minDist = 9999
+    minIndex = 0
+    for p in keypoints:
+        x = p.pt[0] - centerX
+        y = p.pt[1] - centerY
+        distance = np.sqrt(x*x + y*y)
+        print(distance)
+        if distance < minDist:
+            minDist = distance
+            minIndex = i
+        i = i+1
+    target = keypoints[minIndex]
+    return (target.pt[0] - centerX, -1 * (target.pt[1] - centerY))
+
+while(1):
+        ret, frame = cam.read()
+        canvas = frame.copy()
+        frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+        if not ret:
+            break
+
+        
+
+
+        lower = (80,150,150)  #130,150,80
+        upper = (110,255,255) #250,250,120
+        mask = cv2.inRange(frame, lower, upper)
+
+        params = cv2.SimpleBlobDetector_Params()
+        params.filterByColor = True
+        params.blobColor = 255
+        detector = cv2.SimpleBlobDetector_create(params)
+        keypoints = detector.detect(frame)
+        move = findMove(keypoints)
+        print('vector: ', move)
+        canvas = cv2.drawKeypoints(canvas, keypoints,np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+        # cv2.imshow('frame',frame)
+        cv2.imshow('canvas',canvas)
+        # cv2.imshow('mask',mask)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+cam.release()
+cv2.destroyAllWindows()
