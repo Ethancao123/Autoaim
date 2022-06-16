07b9b7ef42c03e8196eb73db9f9507388e7827da
Ethancao123 <ethan.cao123456gmail.comgit config --global user.email ethan.cao123456gmail.com>
2022-06-16 08:09:56 +0100

    worky slowly

--- main.py
+++ main.py
@@ -2,6 +2,89 @@
 import cv2
 import numpy as np;
 
+import serial
+import time
+from datetime import datetime
+
+#all in mm
+#might be adjusted after rollers are added
+topMotorPos = (0, 160) # top motor (X)
+leftMotorPos = (-368.3, -165) # left motor (Y)
+rightMotorPos = (368.3, -165) # right motor (Z)
+dist = 70.5 # attachment point distance
+pulleyCir = 55 * np.pi
+
+currentPos = (0,0)
+
+currentTopMotor = 0
+currentLeftMotor = 0
+currentRightMotor = 0
+
+def distance(p1, p2):
+    return np.sqrt((p1[0]-p2[0]) * (p1[0]-p2[0]) + (p1[1]-p2[1]) * (p1[1]-p2[1]))
+
+# currently is x:(-50,50) y(-25,10)
+def inBoundry(pos):
+    return (pos[0] < 20 and pos[0] > -20 and pos[1] > -10 and pos[1] < 7)
+
+def command(ser, command):
+  start_time = datetime.now()
+  ser.write(str.encode(command)) 
+#   time.sleep(1)
+  print(command)
+  while True:
+    break
+    line = ser.readline()
+    print(line)
+
+    if line == 'b\'ok 0\\r\\n\'':
+      break
+
+#figures out the coordinates of each anchor point
+def recalcPos():
+    if inBoundry(currentPos):
+        xyOffset = dist / np.sqrt(2)
+        global botLeft
+        botLeft = (currentPos[0] - xyOffset, currentPos[1] - xyOffset)
+        global botRight 
+        botRight = (currentPos[0] + xyOffset, currentPos[1] - xyOffset)
+        global top
+        top = (currentPos[0], currentPos[1] + dist)
+        return True
+    else:
+        print("point out of bounds")
+        return False
+
+def movePos(v):
+    global currentPos
+    currentPos = np.add(currentPos, v)
+    print(currentPos)
+    if not recalcPos():
+        currentPos = np.subtract(currentPos, v)
+
+def moveMotors():
+    lenX = distance(topMotorPos, top)
+    lenY = distance(leftMotorPos, botLeft)
+    lenZ = distance(rightMotorPos, botRight)
+    global currentTopMotor 
+    currentTopMotor = lenX - initTop
+    global currentLeftMotor 
+    currentLeftMotor = lenY - initLeft
+    global currentRightMotor
+    currentRightMotor = lenZ - initRight
+    command(ser, "G0 X" + str(-currentTopMotor/10) + " Y" + str(-currentLeftMotor/10) + " Z" + str(-currentRightMotor/10) + "\r\n")
+
+def initSerial():
+    global ser
+    ser = serial.Serial('/dev/ttyUSB0', 115200)
+    time.sleep(4)
+    command(ser, "M17\r\n")
+    command(ser, "G21\r\n")
+    command(ser, "G90\r\n")
+    command(ser, "M92 X200 Y200 Z200\r\n")
+    command(ser, "M203 X30000 Y30000 Z30000\r\n")
+    command(ser, "M204 X17200 Y17200 Z17200\r\n")
+
 # Read image
 cam = cv2.VideoCapture(0)
 # finds the vector the crosshair needs to move to reach the closest target
@@ -25,6 +108,14 @@ def findMove(keypoints):
     target = keypoints[minIndex]
     return (target.pt[0] - centerX, -1 * (target.pt[1] - centerY))
 
+initSerial()
+recalcPos()
+initRight = distance(rightMotorPos, botRight)
+initLeft = distance(leftMotorPos, botLeft)
+initTop = distance(topMotorPos, top)
+movePos((0,0))
+moveMotors()
+input()
 while(1):
         ret, frame = cam.read()
         canvas = frame.copy()
@@ -50,6 +141,9 @@ while(1):
         keypoints = detector.detect(mask)
         move = findMove(keypoints)
         print('vector: ', move)
+        if move != None:
+            movePos((move[0]/100, move[1]/100))
+            moveMotors()
         canvas = cv2.drawKeypoints(canvas, keypoints,np.array([]), (0,0,0), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
         # cv2.imshow('frame',frame)
--- moveMouse.py
+++ moveMouse.py
@@ -2,15 +2,14 @@ import serial
 import time
 from datetime import datetime
 import numpy as np
-import motors
 
 #all in mm
 #might be adjusted after rollers are added
-topMotorPos = (0, 152.4) # top motor (X)
-leftMotorPos = (-368.3, -152.4) # left motor (Y)
-rightMotorPos = (368.3, -152.4) # right motor (Z)
+topMotorPos = (0, 160) # top motor (X)
+leftMotorPos = (-368.3, -165) # left motor (Y)
+rightMotorPos = (368.3, -165) # right motor (Z)
 dist = 70.5 # attachment point distance
-pulleyCir = 60 * np.pi
+pulleyCir = 55 * np.pi
 
 currentPos = (0,0)
 
@@ -19,11 +18,11 @@ currentLeftMotor = 0
 currentRightMotor = 0
 
 def distance(p1, p2):
-    return np.sqrt((p1[0]-p2[0])^2 + (p1[1]-p2[1])^2)
+    return np.sqrt((p1[0]-p2[0]) * (p1[0]-p2[0]) + (p1[1]-p2[1]) * (p1[1]-p2[1]))
 
-# currently is x:(-75,75) y(-85,25)
+# currently is x:(-50,50) y(-25,10)
 def inBoundry(pos):
-    return (pos[0] < 75 && pos[0] > -75 && pos[1] > -85 && pos[1] < 25)
+    return (pos[0] < 50 and pos[0] > -50 and pos[1] > -25 and pos[1] < 10)
 
 def command(ser, command):
   start_time = datetime.now()
@@ -52,7 +51,9 @@ def recalcPos():
         print("point out of bounds")
 
 def movePos(v):
+    global currentPos
     currentPos = np.add(currentPos, v)
+    print(currentPos)
     recalcPos()
 
 def moveMotors():
@@ -60,12 +61,12 @@ def moveMotors():
     lenY = distance(leftMotorPos, botLeft)
     lenZ = distance(rightMotorPos, botRight)
     global currentTopMotor 
-    currentTopMotor = lenX
+    currentTopMotor = lenX - initTop
     global currentLeftMotor 
-    currentLeftMotor = lenY
+    currentLeftMotor = lenY - initLeft
     global currentRightMotor
-    currentRightMotor = lenZ
-    command(ser, "G0 X" + lenX + " Y" + lenY " Z" + lenZ + "\r\n")
+    currentRightMotor = lenZ - initRight
+    command(ser, "G0 X" + str(-currentTopMotor) + " Y" + str(currentLeftMotor) + " Z" + str(currentRightMotor) + "\r\n")
 
 def initSerial():
     global ser
@@ -73,16 +74,24 @@ def initSerial():
     time.sleep(4)
     command(ser, "G21\r\n")
     command(ser, "G90\r\n")
-    command(ser, "M92 X60 Y60 Z60\r\n")
+    command(ser, "M92 X20 Y20 Z20\r\n")
     command(ser, "M203 X30000 Y30000 Z30000\r\n")
     command(ser, "M204 X17200 Y17200 Z17200\r\n")
 
+initSerial()
 recalcPos()
-lenBotLeft = distance(leftMotorPos, botLeft)
-lenBotRight = distance(rightMotorPos, botRight)
-lenTop = distance(topMotorPos, top)
+initRight = distance(rightMotorPos, botRight)
+initLeft = distance(leftMotorPos, botLeft)
+initTop = distance(topMotorPos, top)
 while 1:
-    movePos((int(input('x coord:')),0))
+    input('x coord:')
+    movePos((10,0))
+    moveMotors()
+    movePos((0,5))
+    moveMotors()
+    movePos((-10,0))
+    moveMotors()
+    movePos((0,-5))
     moveMotors()
 
 
