c9e6525bf3a551329329d10fef89f787be51da19
Arjun Dixit <technoarjun@gmail.com>
2022-06-19 23:59:40 -0500

    merge

--- main.py
+++ main.py
@@ -68,17 +68,11 @@ def initSerial():
     command(ser, "M203 X150 Y150 Z150\r\n")
     time.sleep(1)
     command(ser, "M92 Y17 Z37\r\n")
-<<<<<<< HEAD
-    time.sleep(2)
-    command(ser, "M203 Y" + str(1.7) + " Z" + str(37) + "\r\n")
-    time.sleep(2)
-=======
     time.sleep(3)
     command(ser, "M203 Y" + str(1.7*Y_SPEED) + " Z" + str(37*Z_SPEED) + "\r\n")
     time.sleep(1)
     command(ser, "G92 E0\r\n")
     time.sleep(1)
->>>>>>> 74fd1851631ebef45e0478cc369e4537793addb6
     command(ser, "G28 Z0\r\n")
 
     input()
